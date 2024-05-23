33959b75ce84bbed5fd28875ff4b4cd1a8e545c7
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-23 23:44:44 +0530

    1. factorial function

---
+++ js/mathcode.js
@@ -0,0 +1,20 @@
+// factorial math function
+
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    return n * factorial(n-1);
+}
+console.log(factorial(3));
+console.log(factorial(4));
+console.log(factorial(5));
+console.log(factorial(0));
+console.log(factorial(1));
+
+
+
+
+
+
+
